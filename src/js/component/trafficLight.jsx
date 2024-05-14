fa25bff17b8a75d9b2929934b85efda070df005a
kath424 <acostak424@gmail.com>
2024-05-14 23:42:09 +0000

    Modifica el css

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -8,10 +8,22 @@ function TrafficLight(){
     }
 
     return (
-        <div className="container bg-dark py-3" style={{width: "7%"}}>
-            <div className={`border border-danger rounded-circle bg-danger ${color === 'red' ? 'selected' : ''}`} id="red" onClick={btnActiveColor} style={{width:"90px", height: "90px"}}></div>
-            <div className={`border border-warning rounded-circle bg-warning ${color === 'yellow' ? 'selected' : ''}`} id="yellow" onClick={btnActiveColor} style={{width:"90px", height: "90px"}}></div>
-            <div className={`border border-success rounded-circle bg-success ${color === 'green' ? 'selected' : ''}`} id="green" onClick={btnActiveColor} style={{width:"90px", height: "90px"}}></div>
+        <div className="container py-3">
+            <div className="row">
+                <div className="col d-flex justify-content-center">
+                    <div className="line"></div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col d-flex justify-content-center">
+                    <div className="bg-dark container__trafiicLight p-4">
+                        <div className={`border border-danger rounded-circle bg-danger circle__trafiicLight  ${color === 'red' ? 'selected' : ''}`} id="red" onClick={btnActiveColor}></div>
+                        <div className={`border border-warning rounded-circle bg-warning circle__trafiicLight ${color === 'yellow' ? 'selected' : ''}`} id="yellow" onClick={btnActiveColor}></div>
+                        <div className={`border border-success rounded-circle bg-success  circle__trafiicLight ${color === 'green' ? 'selected' : ''}`} id="green" onClick={btnActiveColor}></div>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     )
 }
